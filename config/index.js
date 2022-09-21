86c7ea1eaa6338a0f950fa42b00ac5786ebcacf0
brianlikk <113581398+brianlikk@users.noreply.github.com>
2022-09-21 17:16:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "小李", "year": "1994", "date": "03-25"},
+        {"type": "*生日", "name": "小彬", "year": "1994", "date": "03-25"},
         {"type": "节日", "name": "遇见", "year": "2018", "date": "09-24"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "小丽", "year": "1992", "date": "06-12"},
@@ -59,15 +59,37 @@ export const config = {
     },
     {
       name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
+      id: "ozpeR6oQez9PLIQ3SaAAphPJvWg8",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "22YvnhuSnhLJjGYs854yxyP0gmAVvzrcK8OfiDZhxV8",
+      // 所在省份
+      province: "四川",
+      // 所在城市
+      city: "成都",
+      // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "小彬", "year": "1994", "date": "03-25"},
+        {"type": "节日", "name": "遇见", "year": "2018", "date": "09-24"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "小丽", "year": "1992", "date": "06-12"},
+        {"type": "节日", "name": "相遇的日子", "2020": "2021", "date": "05-01"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2018-09-24"},
+        // 结婚纪念日
+        {"keyword": "marry_day", date: "2022-09-09"},
+        // 退伍日
+        {"keyword": "ex_day", date: "2022-09-10"},
+      ]
     },
     {
       name: "老婆2",
